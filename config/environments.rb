a618122af4b01e0418f6c645e636767674101bff
yiweihuang <n1993212@gmail.com>
2016-07-06 12:13:25 +0800

    API sends and expects auth_token

---
+++ config/environments.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+
+configure :development do
+  ENV['DATABASE_URL'] = 'sqlite://db/dev.db'
+end
+
+configure :test do
+  ENV['DATABASE_URL'] = 'sqlite://db/test.db'
+end
+
+configure :development, :test do
+  require 'config_env'
+  ConfigEnv.path_to_config("#{__dir__}/config_env.rb")
+end
+
+configure do
+  require 'sequel'
+  DB = Sequel.connect(ENV['DATABASE_URL'])
+
+  require 'hirb'
+  Hirb.enable
+end
