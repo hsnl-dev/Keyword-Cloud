c9fa8e7b81f6aeaf5403438754e833375a59a034
yiweihuang <n1993212@gmail.com>
2016-07-06 12:24:23 +0800

    delete API -  post course info

--- controllers/accounts/accounts_controller.rb
+++ controllers/accounts/accounts_controller.rb
@@ -13,18 +13,4 @@ class KeywordCloudAPI < Sinatra::Base
       halt 404
     end
   end
-
-  post '/api/v1/accounts/:uid' do
-    content_type 'application/json'
-    begin
-      uid = params[:uid]
-      halt 401 unless authorized_account?(env, uid)
-      puts 'auth success'
-      courseInfo = FindCourseAuth.call(uid: uid)
-      JSON.pretty_generate(data: courseInfo)
-    rescue => e
-      logger.info "FAILED to find authorized courses for account: #{e}"
-      halt 404
-    end
-  end
 end
