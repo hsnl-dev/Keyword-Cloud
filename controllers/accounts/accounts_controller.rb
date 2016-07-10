97355618b973bd0b983e75e9af7e3031b40b73f9
yiweihuang <n1993212@gmail.com>
2016-07-10 21:43:05 +0800

    delete comment

--- controllers/accounts/accounts_controller.rb
+++ controllers/accounts/accounts_controller.rb
@@ -1,11 +1,9 @@
-# Configuration Sharing Web Service
 class KeywordCloudAPI < Sinatra::Base
   get '/api/v1/accounts/:uid' do
     content_type 'application/json'
     begin
       uid = params[:uid]
       halt 401 unless authorized_account?(env, uid)
-      puts 'auth success'
       courseInfo = FindCourseAuth.call(uid: uid)
       JSON.pretty_generate(data: courseInfo)
     rescue => e
