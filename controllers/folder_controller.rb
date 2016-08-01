8a3d2675666a5ad690a020b25977660618372f5e
yiweihuang <n1993212@gmail.com>
2016-08-01 23:46:25 +0800

    Front-end nav need

--- controllers/folder_controller.rb
+++ controllers/folder_controller.rb
@@ -29,7 +29,7 @@ class KeywordCloudAPI < Sinatra::Base
       folder_type = params[:folder_type]
       halt 401 unless authorized_account?(env, uid)
 
-
+      course_name = Course.where(id: course_id).first.course_name
       folder = Folder.where(course_id: course_id, folder_type: folder_type).all
       folderInfo = folder.map do |s|
         {
@@ -43,7 +43,7 @@ class KeywordCloudAPI < Sinatra::Base
           }
         }
       end
-      JSON.pretty_generate(data: folderInfo)
+      JSON.pretty_generate(course_name: course_name, data: folderInfo)
     rescue => e
       logger.info "FAILED to find secrets for user #{params[:owner_id]}: #{e}"
       halt 404
