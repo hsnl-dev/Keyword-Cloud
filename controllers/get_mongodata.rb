afde605f502b759380c3487a1a21c2819e87bf09
歐博文 <wen94319@gmail.com>
2016-08-01 17:23:31 +0800

    modified get_mongodata.rb/modified get_mongodata_by_cid.rb

--- controllers/get_mongodata.rb
+++ controllers/get_mongodata.rb
@@ -1,12 +1,13 @@
 require 'sinatra'
-require 'json' # required for .to_json
+
+# find a document by its courseId and chapterid
+# testcid = 848
+
 class KeywordCloudAPI < Sinatra::Base
-  # find a document by its courseId and chapterid
-  # testcid = 848  testchapterid = 5209
-  get '/api/v1/mongo/:uid/:course_id/:chapter_id' do
+  get '/api/v1/mongo/:uid/:course_id' do
     content_type :json
     begin
-      data = GetMongoDataByCid.call(course_id: params[:course_id],chapter_id: params[:chapter_id])
+      data = GetMongoDataByCid.call(course_id: params[:course_id])
     rescue => e
       logger.info "FAILED to connect mongodb: #{e}"
       halt 404
