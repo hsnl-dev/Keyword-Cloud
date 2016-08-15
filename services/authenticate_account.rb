a38ee2fd54e4b495a1cb36b074f8a028f949a64a
yiweihuang <n1993212@gmail.com>
2016-08-16 03:01:26 +0800

    use proxy server

--- services/authenticate_account.rb
+++ services/authenticate_account.rb
@@ -1,17 +1,15 @@
-require 'mysql2'
+require 'http'
+require 'base64'
 require 'digest/md5'
 
 # Find account and check password
 class AuthenticateAccount
   def self.call(account:, password:)
     return nil unless account && password
-
-    db = Mysql2::Client.new(host: ENV['HOSTNAME'], username: ENV['USERNAME'],
-                            password: ENV['PASSWORD'], database: ENV['DATABASE'])
-    sql = "SELECT id,password FROM usr_account WHERE email = '#{account}' AND deleted = 0"
-    result = db.query(sql)
-    record = result.first
+    account = Base64.strict_encode64(account)
+    response = HTTP.get("#{ENV['PROXY_API']}/app/account/#{account}")
     encrypt = Digest::MD5.hexdigest(password)
+    record = response.parse
     if record['password'].eql? encrypt
       [record['id'], JWE.encrypt(record['id'].to_s)]
     else
