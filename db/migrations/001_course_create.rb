edfed799a535ac722c8ece19c3b5d55793eae121
yiweihuang <n1993212@gmail.com>
2016-08-22 14:24:52 +0800

    string to fixnum

--- db/migrations/001_course_create.rb
+++ db/migrations/001_course_create.rb
@@ -3,7 +3,7 @@ require 'sequel'
 Sequel.migration do
   change do
     create_table(:courses) do
-      String :id, primary_key: true
+      Integer :id, primary_key: true
       String :course_name
     end
   end
