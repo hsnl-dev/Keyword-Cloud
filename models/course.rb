282da35649093377255f11f5a2b538648e17ecc7
yiweihuang <n1993212@gmail.com>
2016-09-01 03:47:44 +0800

    create keyword db

--- models/course.rb
+++ models/course.rb
@@ -7,9 +7,13 @@ class Course < Sequel::Model
                class: :Folder,
                key: :course_id
 
- one_to_many :course_videourls,
-              class: :Videourl,
-              key: :course_id
+  one_to_many :course_videourls,
+               class: :Videourl,
+               key: :course_id
+
+  one_to_many :course_keywords,
+               class: :Keyword,
+               key: :course_id
 
   def to_json(options = {})
     JSON({  type: 'courses',
