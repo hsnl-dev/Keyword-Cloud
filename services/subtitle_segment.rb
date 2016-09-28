776b6bdd7b2b6dd30eb77ddba300ddfac7673ce9
yiweihuang <n1993212@gmail.com>
2016-09-28 13:53:07 +0800

    add subtitle and student action

--- services/subtitle_segment.rb
+++ services/subtitle_segment.rb
@@ -1,14 +1,15 @@
 require 'base64'
 
-class SlideSegment
-  def self.call(folder_id:)
+class SubtitleSegment
+  def self.call(folder_id:, slide:)
     doc = SimpleFile.where(folder_id: folder_id)
                     .all
     fileInfo = doc.map do |s|
       plain = Base64.strict_decode64(s.document)
       decoded = plain.force_encoding('UTF-8')
-      `python3 helpers/slide_segment.py "#{decoded}"`
+      `python3 helpers/subtitle_segment.py "#{decoded}" "#{slide}"`
     end
+    puts fileInfo
     fileInfo
   end
 end
