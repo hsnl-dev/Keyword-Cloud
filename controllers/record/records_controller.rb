16069aa40eeafcb4f7384638a523abfeddbc5bb7
yiweihuang <n1993212@gmail.com>
2016-11-24 09:28:26 +0800

    if it don't have slide, the api of the mix_keyword will work

--- controllers/record/records_controller.rb
+++ controllers/record/records_controller.rb
@@ -5,7 +5,7 @@ class KeywordCloudAPI < Sinatra::Base
     content_type 'application/json'
     begin
       FindVideoRecord.call(course_id: params[:course_id])
-      JSON.pretty_generate(status: 'success')
+      JSON.pretty_generate(status: 'mongo success')
     rescue => e
       logger.info "FAILED to connect mongodb: #{e}"
       halt 404
@@ -19,7 +19,7 @@ class KeywordCloudAPI < Sinatra::Base
       course_arr.map do |id|
         FindChapterVideo.call(course_id: id)
       end
-      JSON.pretty_generate(data: 'success')
+      JSON.pretty_generate(data: 'videos success')
     rescue => e
       logger.info "FAILED to get chapter video: #{e}"
       halt 404
@@ -48,12 +48,23 @@ class KeywordCloudAPI < Sinatra::Base
     content_type 'application/json'
     begin
       course_id = params[:course_id]
+      # coursename = Course.where(id: course_id).first.course_name
       chid = FindCourseKeyword.call(course_id: course_id)
       keywordInfo = chid.map do |id, contents|
         folderInfo = Folder.where(course_id: course_id, chapter_id: id, folder_type: 'subtitles').first
         if Keyword.where(chapter_id: id, folder_type: 'slides').first != nil
           slide_keyword = Keyword.where(chapter_id: id, folder_type: 'slides').first.keyword
           json = CreateFinalKeyword.call(slide_keyword: slide_keyword, subtitle_keyword: contents)
+          CreateKeywordForChap.call(
+            course_id: course_id,
+            folder_id: folderInfo.id,
+            chapter_id: id,
+            chapter_name: folderInfo.name,
+            folder_type: 'subtitles',
+            priority: 1,
+            keyword: json)
+        else
+          json = CreateFinalKeyword.call(slide_keyword: 'empty', subtitle_keyword: contents)
           CreateKeywordForChap.call(
             course_id: course_id,
             folder_id: folderInfo.id,
@@ -85,7 +96,12 @@ class KeywordCloudAPI < Sinatra::Base
   get '/api/v1/shellscript' do
     content_type 'application/json'
     begin
-      course_id = Keyword.select(:course_id).map(&:course_id).uniq
+      folder_id = Subtitle.select(:folder_id).map(&:folder_id).uniq
+      course_id = folder_id.map do |id|
+        Folder.where(id: id).first.course_id
+      end
+      course_id = course_id.uniq
+      # course_id = Folder.select(:course_id).map(&:course_id).uniq
       shell_array = course_id.map { |i| i.to_s }.join(" ")
       JSON.pretty_generate(data: shell_array)
     rescue => e
